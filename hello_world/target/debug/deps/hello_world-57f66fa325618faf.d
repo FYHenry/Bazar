ec7e1a576c8e6243254181091f85ad7ac42e2920
Alnotz <fredh0001@net-c.fr>
2022-07-23 20:30:50 +0200

    To remove

--- hello_world/target/debug/deps/hello_world-57f66fa325618faf.d
+++
@@ -1,5 +0,0 @@
-/home/fh/Git/Alnotz/Bazar/hello_world/target/debug/deps/hello_world-57f66fa325618faf: src/main.rs
-
-/home/fh/Git/Alnotz/Bazar/hello_world/target/debug/deps/hello_world-57f66fa325618faf.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/fh/Git/Alnotz/Bazar/hello_world/target/debug/hello_world: /home/fh/Git/Alnotz/Bazar/hello_world/src/main.rs
